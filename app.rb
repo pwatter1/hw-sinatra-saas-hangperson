7a4cd674dd3b53e4ddc676831f6f455ab8a1911a
Patrick Watters <pwatter1@binghamton.edu>
2017-09-17 22:16:24 +0000

    messing with /show

--- app.rb
+++ app.rb
@@ -55,7 +55,13 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    erb :new
+    ### YOUR CODE HERE ###
+    status = @game.check_win_or_lose
+    if status == :win
+      redirect '/win'
+    if status == :lose
+      redirect '/lose'
+    
     erb :show # You may change/remove this line
   end
   
